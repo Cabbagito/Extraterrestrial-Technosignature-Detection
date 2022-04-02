ef54534777a42cf25042a00ff78f1f972e9a41d9
Cabbagito <petar.kupusarevic@gmail.com>
2022-04-02 21:51:05 +0200

    Added Augmentation

--- DataProcessing/DataLoader.py
+++ DataProcessing/DataLoader.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 import numpy as np
 from torch import Tensor
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from random import uniform
 
 
 class DataLoader:
@@ -13,6 +16,10 @@ class DataLoader:
         self.n_n = self.n - self.n_a
         self.__loader_initialized = False
         self.use_gpu = False
+        self.augment = False
+        self.augment_pct = 0.25
+        self.augment_validation = False
+        self.augment_validation_pct = 0.25
 
     def load_one(self, filename):
         for dir in os.listdir(self.path):
@@ -56,7 +63,7 @@ class DataLoader:
         n_val = n_val_batches * n_n_per_batch
 
         print(
-            f"Per Batch: {n_per_batch} \nAnomalies Per Batch: {n_a_per_batch} \nNormal Per Batch: {n_n_per_batch} \nValidation Set size: {n_val}"
+            f"Per Batch: {n_per_batch} \nAnomalies Per Batch: {n_a_per_batch} \nNormal Per Batch: {n_n_per_batch} \nValidation Set size: {n_val}\n*Without augmentation*"
         )
 
         self.batches = []
@@ -89,6 +96,8 @@ class DataLoader:
             data.drop(batch_n_df.index, inplace=True)
             data.drop(batch_a_df.index, inplace=True)
 
+        return n_per_batch, n_n_per_batch, n_a_per_batch, n_val
+
     def next_batch(self):
         if not self.__loader_initialized:
             raise Exception("Loader not initialized")
@@ -97,7 +106,9 @@ class DataLoader:
             self.batch_idx = 0
 
         self.batch_idx += 1
-        data = self.__load_batch(self.batches[self.batch_idx - 1])
+        data = self.__load_batch(
+            self.batches[self.batch_idx - 1], self.augment, self.augment_pct
+        )
         if self.use_gpu:
             return Tensor(data[0]).cuda(), Tensor(data[1]).unsqueeze(1).cuda()
         else:
@@ -111,27 +122,65 @@ class DataLoader:
             self.val_batch_idx = 0
 
         self.val_batch_idx += 1
-        data = self.__load_batch(self.val_batches[self.val_batch_idx - 1])
+        data = self.__load_batch(
+            self.val_batches[self.val_batch_idx - 1],
+            self.augment_validation,
+            self.augment_validation_pct,
+        )
         if self.use_gpu:
             return Tensor(data[0]).cuda(), Tensor(data[1]).unsqueeze(1).cuda()
         else:
             return Tensor(data[0]), Tensor(data[1].unsqueeze(1))
 
-    def __load_batch(self, batch):
+    def __load_batch(self, batch, augment=False, aug_pct=0.0):
         retdata = []
         labels = []
         for dir in os.listdir(self.path):
             for file in os.listdir(os.path.join(self.path, dir)):
                 if file[:-4] in batch:
-                    retdata.append(
-                        np.expand_dims(
-                            np.load(os.path.join(self.path, dir, file)), axis=0
-                        )
+
+                    label = self.labels[self.labels["id"] == file[:-4]][
+                        "target"
+                    ].values[0]
+
+                    value = np.expand_dims(
+                        np.load(os.path.join(self.path, dir, file)), axis=0
                     )
-                    labels.append(
-                        self.labels[self.labels["id"] == file[:-4]]["target"].values[0]
-                    )
+                    retdata.append(value)
+                    labels.append(label)
+
+                    if augment and label == 1:
+                        augment_data = self.augment_img(value, aug_pct)
+                        for aug_value in augment_data:
+                            retdata.append(aug_value)
+                            labels.append(label)
+
                     if len(retdata) == len(batch):
                         break
 
         return (np.array(retdata), labels)
+
+    def augment_img(self, x, aug_pct=0.0):
+        retdata = []
+
+        if uniform(0, 1) < aug_pct:
+            x1 = np.fliplr(x[0])
+            x1 = np.expand_dims(x1, 0)
+            retdata.append(x1)
+
+        if uniform(0, 1) < aug_pct:
+            x2 = np.flipud(x[0])
+            x2 = np.expand_dims(x2, 0)
+            retdata.append(x2)
+
+        if uniform(0, 1) < aug_pct:
+            x3 = ndimage.rotate(x[0], 5, reshape=False)
+            x3 = np.expand_dims(x3, 0)
+            retdata.append(x3)
+
+        if uniform(0, 1) < aug_pct:
+            x4 = ndimage.rotate(x[0], -5, reshape=False)
+            x4 = np.expand_dims(x4, 0)
+            retdata.append(x4)
+
+        return retdata
--- ImageClassificationModel/training.py
+++ ImageClassificationModel/training.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from torch import Tensor
-import torch.optim as optim
-import torch
+from torch.optim import Adam, AdamW, RMSprop
+from torch import save, load
+from torch.nn import BCELoss
+
 
 import time
 
@@ -11,23 +12,38 @@ sys.path.append(os.path.join(os.getcwd()))
 from DataProcessing.dataLoader import DataLoader
 from model import Model
 
+
 ###
 DATADIR = "../Data/train"
+MODELS_DIR = "ImageClassificationModel/models"
 
 N_FULL = 60000
 N_ANOMALY = 6000
 N_NORMAL = 54000
 IMG_SHAPE = (1, 819, 256)
+###
 
-USE_GPU = False
+USE_GPU = True
 N_BATCHES = 500
 N_VAL_BATCHES = 1
 NORMAL_PCT = 0.7
+
+AUGMENT_TRAINING_DATA = False
+AUGMENT_TRAINING_DATA_PERCENTAGE = 0.25
+AUGMENT_TESTING_DATA = False
+AUGMENT_TESTING_DATA_PERCENTAGE = 0.25
+
+###
+
+LEARNING_RATE = 0.001
+
 
 ###
 
 model = Model()
 loader = DataLoader(DATADIR)
+loss = BCELoss()
+optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
 
 n_params = sum(p.numel() for p in model.parameters())
 n_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -44,26 +60,13 @@ print(
     f"Number of parameters: {n_params}\nNumber of trainable parameters: {n_trainable_params}\n\n"
 )
 
-"""  Load Dummy Data Test
-dummy_size = 100
-start = time.time()
-dummy_data = loader.load_n(dummy_size)
-end = time.time()
 
-print(f"Loading {dummy_size} observations took {end - start} seconds")
+loader.augment = AUGMENT_TRAINING_DATA
+loader.augment_pct = AUGMENT_TRAINING_DATA_PERCENTAGE
+loader.augment_validation = AUGMENT_TESTING_DATA
+loader.augment_validation_pct = AUGMENT_TESTING_DATA_PERCENTAGE
 
-"""
-loader.start_loader(
+n_per_batch, n_n_per_batch, n_a_per_batch, n_val = loader.start_loader(
     n_batches=N_BATCHES, normal_pct=NORMAL_PCT, n_val_batches=N_VAL_BATCHES
 )
-
-x, yhat = loader.next_batch()
-
-
-start = time.time()
-y = model.forward(x)
-end = time.time()
-
-
-print(f"Forward Pass of size {dummy_size} took {end - start} seconds")
 
